46f70401097c261838f49ab088a7d4a008a8e958
Patra3 <czheng1968@gmail.com>
2021-05-22 19:53:06 -0400

    Huge work!

--- Print.java
+++ Print.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
@@ -13,7 +15,7 @@ import org.jnativehook.NativeHookException;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class Print{
+public class Print {
   
   private JSONObject menu;
   private DiscordApi bot = new DiscordApiBuilder().setToken("ODQyMTQ4MjIwMDM3NzU5MDI3.YJxFpg.Z3rBemeRV3vsiUNridUqtwAtwXE").login().join();
@@ -21,6 +23,7 @@ public class Print{
   private static MouseListener listener = new MouseListener();
   private String registeringItem;
   private String loc = System.getProperty("user.home") + "/Desktop/";
+  private JSONObject fx;
 
   Print() {
     try {
@@ -83,39 +86,137 @@ public class Print{
           }
           
         }
-
+        else{
+          String[] messageParts = event.getMessageContent().split(" ");
+          if (messageParts[0].equalsIgnoreCase("!click")){
+            // Item is mp[1]
+            // We need to find the type first.
+            JSONObject i = getMenuItem(messageParts[1]);
+            clickItem(messageParts[1], i.getInt("type"), "");
+            event.getChannel().sendMessage("Click sent!");
+          }
+        }
       });
-
-      System.out.println(getAllItemsInCategory(3));
-      clickItem("L1", 0);
     }
     catch(FileNotFoundException e){
       e.printStackTrace();
     }
   }
 
+  public JSONObject getMenuItem(String code){
+    menu.getJSONArray("items").forEach(item -> {
+      JSONObject i = (JSONObject)item;
+      if (i.getString("code").equals(code))
+        fx = i;
+    });
+    return this.fx;
+  }
+
   /**
    * Executes a click on an item.
    * @param code i.e.  C4
    * @param type i.e.  Lunch Special = 0
+   * @param choice i.e. Pork, chicken
    */
-  public void clickItem(String code, int type){
+  public void clickItem(String code, int type, String choice){
+
+    // Click on the category first.
+    click("type_" + type);
+
     // TO-DO: Enter edge cases manually here.
 
+
     /*
-    
-    [1] [2] [3] [4]
-    [5] [6] [7] [8]
-    ... 
-    
+    List of Hard codes needed:
+    - Lunch Special [done]
+    - Special Combo [done]
+    - Appetizers
+    - Lo Mein
+    - Chow Mei Fun
+    - Chicken
+    - Beef
+    - Shrimp
+    - Vegetables
+    - Chef Special
+    - Side Order
     */
-    // Get the exact grid position.
-    int position = locationWithinCategory(code, type);
 
-    // Get the number of rows need to shift down.
-    int shiftsDown = numberOfShiftsDown(position);
+    if (type == 0 || type == 12){
+      // Lunch Special && Special Combo hard code
+      JSONObject ar = getMenuItem(code);
+      String name = code + ". " + ar.getString("name");
+      if (ar.has("choice")){
+        name += "_" + choice;
+      }
+      click(name);
+    }
 
-    
+    else{
+
+      /*
+      
+      [1] [2] [3] [4]
+      [5] [6] [7] [8]
+      ... 
+      
+      */
+      // Get the exact grid position.
+      int position = locationWithinCategory(code, type);
+
+      // Click on the item.
+      click("grid_" + (position + 1));
+    }
+  }
+
+  /**
+   * Mouse click at point.
+   */
+  public void click(int x, int y){
+    try{
+      Robot bot = new Robot();
+      bot.mouseMove(x, y);
+      bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+      bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    }
+    catch(Exception e){
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Deliver a click to an item.
+   * @param access
+   * @return
+   */
+  public void click(String access){
+    System.out.println(access);
+    // [    [0,0] ,  [1,0]  , [2,0]    ]
+    ArrayList<ArrayList<Integer>> t = new ArrayList<>();
+    try{
+      File payload = new File(loc + "positions/" + access + ".txt");
+      Scanner scanner = new Scanner(payload);
+      String data = "";
+      while (scanner.hasNextLine()) {
+        data += scanner.nextLine();
+      }
+      scanner.close();
+      String[] datum = data.split(";");
+      for (String entry : datum){
+        // entry would be 370,88 for example
+        String[] xy = entry.split(",");
+        ArrayList<Integer> coordinates = new ArrayList<>();
+        for (String num : xy)
+          coordinates.add(Integer.parseInt(num));
+        t.add(coordinates);
+      }
+    }
+    catch(FileNotFoundException e){
+      e.printStackTrace();
+    }
+    for (ArrayList<Integer> point : t){
+      int x = point.get(0), y = point.get(1);
+      click(x, y);
+    }
   }
 
   /**
