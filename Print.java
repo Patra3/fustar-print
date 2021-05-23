1ce52155337ae0fd92e24a51b23ff6c8c58f99b1
Chen Zheng <czheng1968@gmail.com>
2021-05-23 15:16:40 -0400

    Wow!

--- Print.java
+++ Print.java
@@ -117,32 +117,26 @@ public class Print {
    * @param code i.e.  C4
    * @param type i.e.  Lunch Special = 0
    * @param choice i.e. Pork, chicken
+   * @param size i.e. Small
    */
   public void clickItem(String code, int type, String choice){
 
     // Click on the category first.
-    click("type_" + type);
-
-    // TO-DO: Enter edge cases manually here.
+    try{
+      click("type_" + type);
+    }
+    catch(FileNotFoundException e){
+      e.printStackTrace();
+    }
 
 
     /*
-    List of Hard codes needed:
-    - Lunch Special [done]
-    - Special Combo [done]
-    - Appetizers
-    - Lo Mein
-    - Chow Mei Fun
-    - Chicken
-    - Beef
-    - Shrimp
-    - Vegetables
-    - Chef Special
-    - Side Order
+    The plan is to:
+    - Search for a precise item file name for hardcode,
+    - if not found, then use grid fallback.
     */
 
-    if (type == 0 || type == 12){
-      // Lunch Special && Special Combo hard code
+    try {
       JSONObject ar = getMenuItem(code);
       String name = code + ". " + ar.getString("name");
       if (ar.has("choice")){
@@ -150,24 +144,137 @@ public class Print {
       }
       click(name);
     }
-    else if (type == 1){
-      // Appetizer hard code
-    }
-
-    else{
-
-      /*
-      
-      [1] [2] [3] [4]
-      [5] [6] [7] [8]
-      ... 
-      
-      */
-      // Get the exact grid position.
-      int position = locationWithinCategory(code, type);
-
-      // Click on the item.
-      click("grid_" + (position + 1));
+    catch(FileNotFoundException e){
+      // manual fixes for Appetizer
+      //(locationWithinCategory(code, type) + 1)
+      String target = "grid_";
+      if (code.equals("6") && choice.equals("Fried")){
+        target += 6;
+      }
+      else if (code.equals("6") && choice.equals("Steamed")){
+        target += 7;
+      }
+      else if (code.equals("7")){
+        target += 8;
+      }
+      else if (code.equals("8")){
+        target += 9;
+      }
+      else if (code.equals("9")){
+        target += 10;
+      }
+      else if (code.equals("10")){
+        target += 11;
+      }
+      else if (code.equals("11")){
+        target += 12;
+      }
+      else if (code.equals("12")){
+        target += 13;
+      }
+      else if (code.equals("13")){
+        target += 14;
+      }
+      else if (code.equals("14")){
+        target += 15;
+      }
+      //manual fixes for chow mei fun.
+      else if (code.equals("37") && choice.equals("Chicken")){
+        target += 3;
+      }
+      else if (code.equals("37") && choice.equals("Roast Pork")){
+        target += 4;
+      }
+      else if (code.equals("38")){
+        target += 5;
+      }
+      else if (code.equals("39")){
+        target += 6;
+      }
+      // Manual fix for chicken.
+      else if (code.equals("40")){
+        target = "40. Chicken with Black Bean Sauce";
+      }
+      // Manual fix for Shrimp.
+      else if (code.equals("92") && choice.equals("Scallop")){
+        target += 12;
+      }
+      else if (code.equals("92") && choice.equals("Shrimp")){
+        target += 13;
+      }
+      else if (code.equals("93")){
+        target += 14;
+      }
+      else if (code.equals("94")){
+        target += 15;
+      }
+      else if (code.equals("95")){
+        target += 16;
+      }
+      else if (code.equals("96")){
+        target += 17;
+      }
+      else if (code.equals("97")){
+        target += 18;
+      }
+      // Manual fix for Vegetable
+      else if (code.equals("105") && choice.equals("Mushroom")){
+        target += 8;
+      }
+      else if (code.equals("105") && choice.equals("Vegetable")){
+        target += 9;
+      }
+      else if (code.equals("106")){
+        target += 10;
+      }
+      // Manual fix for Chef specials
+      else if (code.equals("S12") && choice.equals("Scallop")){
+        target += 12;
+      }
+      else if (code.equals("S12") && choice.equals("Shrimp")){
+        target += 13;
+      }
+      else if (code.equals("S12")){
+        target += 13; // Assume shrimp?
+      }
+      else if (code.equals("S13")){
+        target += 14;
+      }
+      else if (code.equals("S14")){
+        target += 15;
+      }
+      else if (code.equals("S15")){
+        target += 16;
+      }
+      else if (code.equals("S16")){
+        target += 17;
+      }
+      // TO-DO: Side order!
+      else {
+        // Test only with number parsing.
+        try {
+          Integer itemCode = Integer.parseInt(code);
+          // Manual fix for chicken.
+          if (itemCode > 40 && itemCode < 62){
+            target += locationWithinCategory(code, type);
+          }
+          else{
+            // For non-fix-needed items.
+            target += locationWithinCategory(code, type) + 1;
+          }
+        }
+        catch(Exception x){
+          // Move on, not a number.
+          target += locationWithinCategory(code, type) + 1;
+          System.out.println("Item not number, moving on...");
+        }
+      }
+      try {
+        click(target);
+      }
+      catch(FileNotFoundException f){
+        f.printStackTrace();
+      }
     }
   }
 
@@ -191,30 +298,25 @@ public class Print {
    * @param access
    * @return
    */
-  public void click(String access){
+  public void click(String access) throws FileNotFoundException{
     System.out.println(access);
     // [    [0,0] ,  [1,0]  , [2,0]    ]
     ArrayList<ArrayList<Integer>> t = new ArrayList<>();
-    try{
-      File payload = new File(loc + "positions/" + access + ".txt");
-      Scanner scanner = new Scanner(payload);
-      String data = "";
-      while (scanner.hasNextLine()) {
-        data += scanner.nextLine();
-      }
-      scanner.close();
-      String[] datum = data.split(";");
-      for (String entry : datum){
-        // entry would be 370,88 for example
-        String[] xy = entry.split(",");
-        ArrayList<Integer> coordinates = new ArrayList<>();
-        for (String num : xy)
-          coordinates.add(Integer.parseInt(num));
-        t.add(coordinates);
-      }
+    File payload = new File(loc + "positions/" + access + ".txt");
+    Scanner scanner = new Scanner(payload);
+    String data = "";
+    while (scanner.hasNextLine()) {
+      data += scanner.nextLine();
     }
-    catch(FileNotFoundException e){
-      e.printStackTrace();
+    scanner.close();
+    String[] datum = data.split(";");
+    for (String entry : datum){
+      // entry would be 370,88 for example
+      String[] xy = entry.split(",");
+      ArrayList<Integer> coordinates = new ArrayList<>();
+      for (String num : xy)
+        coordinates.add(Integer.parseInt(num));
+      t.add(coordinates);
     }
     for (ArrayList<Integer> point : t){
       int x = point.get(0), y = point.get(1);
